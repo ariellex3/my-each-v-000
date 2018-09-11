4a57ec607d31c8229210d2c1f9074ecba66989dd
Arielle D <domingu4@gmail.com>
2018-09-11 01:39:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,8 @@
 def my_each(words)
-  if block_given?
     i = 0
     while i < words.length
       yield words[i]
       i = i + 1
     end
     words
-  else
-    puts "No block given."
-  end
 end
