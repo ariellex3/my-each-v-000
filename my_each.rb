1d605c00b4b722c3be008a6cbc688e1286745093
Arielle D <domingu4@gmail.com>
2018-09-11 01:39:30 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,35 @@
 def my_each(words)
+  if block_given?
+    i = 0
+    while i < words.length
+      yield words[i]
+      i = i + 1
+    end
+    words
+  else
+    puts "No block given."
+  end
+    
 
+end
+
+
+
+
+def hello_t(array)
+  if block_given?
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
+  array
+else puts "Hey! No block was given!"
+end
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
